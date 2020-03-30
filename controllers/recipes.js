c756f7f0934d4cba4198cae5f381ca807e9d3ad1
Jeff Elkins <elkins2k@mail.com>
2020-03-30 13:47:53 -0500

    tried Marcs solution for recipes.post

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -17,41 +17,41 @@ router.get('/:id', (req, res) => {
     .then(single => res.json(single))
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const newRecipe = {
     name: req.body.name,
     directions: req.body.directions,
     submittedBy: req.body.submittedBy
   }
-  Chapter.findOne({mainProtein: req.body.mainProtein})
-    .then(chapter => {
-      if (!chapter) {
-        Chapter
-        .create({mainProtein:req.body.mainProtein})
-        .then(newChapter => {
-          newRecipe.mainProtein = newChapter._id
-          Recipe
-            .create(newRecipe)
-            .then(recipe => res.json(recipe))
-      })
-    } else {
-      newRecipe.mainProtein = chapter._id
-        Recipe
-          .create(newRecipe)
-          .then(recipe => res.json(recipe))
-    }
-  })
-  // let chapter = async Chapter.findOne({mainProtein: req.body.mainProtein})
-  //   if (!chapter._id) {
-  //       let newChapter = await Chapter.create({mainProtein:req.body.mainProtein})
-  //       newRecipe.mainProtein = newChapter._id
-  //       let newlyCreatedRecipe = await Recipe.create(newRecipe)
-  //       res.json(newlyCreatedRecipe)
+  // Chapter.findOne({mainProtein: req.body.mainProtein})
+  //   .then(chapter => {
+  //     if (!chapter) {
+  //       Chapter
+  //       .create({mainProtein:req.body.mainProtein})
+  //       .then(newChapter => {
+  //         newRecipe.mainProtein = newChapter._id
+  //         Recipe
+  //           .create(newRecipe)
+  //           .then(recipe => res.json(recipe))
+  //     })
   //   } else {
-  //       newRecipe.mainProtein = chapter._id
-  //       let newlyCreatedRecipe = await Recipe.create(newRecipe)
-  //       res.json(newlyCreatedRecipe)
+  //     newRecipe.mainProtein = chapter._id
+  //       Recipe
+  //         .create(newRecipe)
+  //         .then(recipe => res.json(recipe))
   //   }
+  // })
+  let chapter = await Chapter.findOne({mainProtein: req.body.mainProtein})
+    if (!chapter._id) {
+        let newChapter = await Chapter.create({mainProtein:req.body.mainProtein})
+        newRecipe.mainProtein = newChapter._id
+        let newlyCreatedRecipe = await Recipe.create(newRecipe)
+        res.json(newlyCreatedRecipe)
+    } else {
+        newRecipe.mainProtein = chapter._id
+        let newlyCreatedRecipe = await Recipe.create(newRecipe)
+        res.json(newlyCreatedRecipe)
+    }
 })
 
 router.put('/:id', (req, res) => {
